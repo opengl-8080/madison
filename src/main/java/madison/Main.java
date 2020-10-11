a1060ebbbc4b4d27d887295db2fa531d3f1d35f9
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-10-11 12:30:09 +0900

    ホームディレクトリを環境変数で設定できるように修正

--- src/main/java/madison/Main.java
+++ src/main/java/madison/Main.java
@@ -3,6 +3,10 @@ package madison;
 import gl.javafx.FxWindow;
 import gl.javafx.Fxml;
 import gl.util.ObjectContainer;
+import gl.util.config.CompositeConfigResolver;
+import gl.util.config.ConfigResolver;
+import gl.util.config.EnvironmentVariableConfigResolver;
+import gl.util.config.SystemPropertyConfigResolver;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
@@ -13,6 +17,8 @@ import madison.persistence.flick.CsvFlickAimRecordRepository;
 import madison.persistence.tracking.CsvTrackingAimRecordRepository;
 import madison.util.MadisonHome;
 
+import java.util.List;
+
 public class Main extends Application {
     
     public static void main(String[] args) {
@@ -21,8 +27,14 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        final CsvFlickAimRecordRepository csvFlickAimRecordRepository = new CsvFlickAimRecordRepository(MadisonHome.getInstance().flickAimRecordCsv());
-        final CsvTrackingAimRecordRepository trackingAimRecordRepository = new CsvTrackingAimRecordRepository(MadisonHome.getInstance().trackingAimRecordCsv());
+        final ConfigResolver resolver = new CompositeConfigResolver(
+                List.of(new EnvironmentVariableConfigResolver(), new SystemPropertyConfigResolver())
+        );
+
+        final MadisonHome madisonHome = new MadisonHome(resolver);
+
+        final CsvFlickAimRecordRepository csvFlickAimRecordRepository = new CsvFlickAimRecordRepository(madisonHome.flickAimRecordCsv());
+        final CsvTrackingAimRecordRepository trackingAimRecordRepository = new CsvTrackingAimRecordRepository(madisonHome.trackingAimRecordCsv());
         
         ObjectContainer.getInstance().register(FlickAimRecordRepository.class, csvFlickAimRecordRepository);
         ObjectContainer.getInstance().register(TrackingAimRecordRepository.class, trackingAimRecordRepository);
--- src/main/java/madison/util/MadisonHome.java
+++ src/main/java/madison/util/MadisonHome.java
@@ -1,26 +1,24 @@
 package madison.util;
 
+import gl.util.Directory;
+import gl.util.config.ConfigResolver;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MadisonHome {
-    private static final MadisonHome instance = new MadisonHome();
-    
-    public static MadisonHome getInstance() {
-        return instance;
+    private final Directory dir;
+
+    public MadisonHome(ConfigResolver configResolver) {
+        final String path = configResolver.resolveRequired("madison.home");
+        this.dir = new Directory(Paths.get(path), Directory.InitializeStrategy.CREATE);
     }
     
-    private final Path home = Paths.get(".");
-    
     public Path flickAimRecordCsv() {
-        return home.resolve("flick-aim-record.csv");
+        return dir.path().resolve("flick-aim-record.csv");
     }
     
     public Path trackingAimRecordCsv() {
-        return home.resolve("tracking-aim-record.csv");
-    }
-    
-    private MadisonHome() {
-        System.out.println(home.toAbsolutePath());
+        return dir.path().resolve("tracking-aim-record.csv");
     }
 }
